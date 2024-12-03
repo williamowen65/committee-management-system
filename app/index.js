de09ea25cd977c4cd9f2e5df32958be795f4404e
William Owen <william.owen.bsc@gmail.com>
2024-12-03 11:03:39 -0800

    Add Firebase integration and update members page with script reference

--- app/index.js
+++ app/index.js
@@ -3,3 +3,4 @@ import "./components/header/index.js";
 import "./components/markdown/index.js";
 import "./components/dual-login-form/index.js";
 import './styles.scss'  // Import the styles
+
---
+++ dist/firebase.js
@@ -0,0 +1,54 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js'
+
+  // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+  import { getAnalytics } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js'
+
+  // Add Firebase products that you want to use
+  import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+  } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js'
+  import {
+    getFirestore,
+    getDocs,
+    collection,
+    query,
+    where,
+    doc,
+    getDoc,
+  } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js'
+  const firebaseConfig = {
+    apiKey: 'AIzaSyAPrNIc_STwgScLfFOfEN8YyGENoHfT6T4',
+    authDomain: 'ghost-d319b.firebaseapp.com',
+    projectId: 'ghost-d319b',
+    storageBucket: 'ghost-d319b.firebasestorage.app',
+    messagingSenderId: '162890047542',
+    appId: '1:162890047542:web:d7ed1c15e94469e0663897',
+    measurementId: 'G-VCKQCEBVRJ',
+  }
+
+  // Initialize Firebase
+  // FIRE BASE SETUP
+
+  const app = initializeApp(firebaseConfig)
+  const analytics = getAnalytics(app)
+  const auth = getAuth(app)
+  const db = getFirestore()
+
+  window.firebase = {
+    auth,
+    db,
+    app,
+    analytics,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    getDocs,
+    getDoc,
+    collection,
+    query,
+    where,
+    doc,
+  }
