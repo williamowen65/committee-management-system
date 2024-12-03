9a7011f5756d1f6455fad33a779792f044167568
William Owen <william.owen.bsc@gmail.com>
2024-12-03 11:38:41 -0800

    Implement dual login form toggle functionality and update input aliases for better user experience

--- app/components/dual-login-form/index.js
+++ app/components/dual-login-form/index.js
@@ -6,5 +6,19 @@ import styles from './style.scss.txt';
 
 createCustomElement('dual-login-form-component', function () {
 
+    setUXEventListeners.bind(this)();
 
 }, dualLoginForm, styles);
+
+
+function setUXEventListeners(){
+     // Change form from signup to login
+     const dividerEl = this.querySelector('.os-dual-form')
+     this.querySelectorAll('.toggleAuthType').forEach((el) => {
+         el.addEventListener('click', (e) => {
+             e.stopPropagation()
+             dividerEl.getAttribute('auth-mode') === 'login' ? dividerEl.setAttribute('auth-mode', 'signup') : dividerEl.setAttribute('auth-mode', 'login')
+             writeFriendlyMessage();
+         })
+     })
+}
