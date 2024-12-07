baaf5be43dd011d7652b4485517012913ec6961c
William Owen <william.owen.bsc@gmail.com>
2024-12-07 08:31:10 -0800

    Refactor dual-login form styles to use flexbox layout and improve button alignment

--- app/components/input/index.js
+++ app/components/input/index.js
@@ -9,37 +9,14 @@ import styles from './style.scss.txt';
 createCustomElement('input-component', function () {
     console.log('input-component loaded');
     // set slot
-    // Evaluate the template (initially with default context)
-    // const defaultContext = {
-    //     fieldName: this.getAttribute('fieldName') || 'defaultFieldName',
-    //     alias: this.getAttribute('alias') || 'defaultAlias',
-    //     capitalizeFirstLetter: (str) => str.charAt(0).toUpperCase() + str.slice(1),
-    //     required: this.getAttribute('required') || false,
-    //     type: this.getAttribute('type') || 'text',
-    // };
-
-    // const evaluatedTemplate = evaluateTemplate(inputTemplate, defaultContext);
-
-    // this.querySelector('input').setAttribute('type', defaultContext.type);
-    // if(defaultContext.required) {
-    // this.querySelector('input').setAttribute('required', true);
-    // }
 
     moveLabel.bind(this)();
 
-    // // Set listeners on all inputs for the label to float
-
-
-
-    // console.log("evaluatedTemplate", evaluatedTemplate);
-    // console.log({ styles })
 
 }, inputTemplate, styles);
 
 createCustomElement('textarea-component', function () {
     moveLabel.bind(this)();
-
-
 }, textareaTemplate, styles);
 
 
