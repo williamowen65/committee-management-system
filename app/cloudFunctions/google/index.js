b1077b67d12b7c1e03e619fa36a161b8521e6c45
William Owen <william.owen.bsc@gmail.com>
2024-12-12 07:32:51 -0800

    Add Google Cloud Functions integration with Express and Gmail API support

--- app/cloudFunctions/google/index.js
+++ app/cloudFunctions/google/index.js
@@ -1,8 +1,40 @@
 const functions = require('@google-cloud/functions-framework');
 const { google } = require('googleapis');
+const express = require('express');
+const fs = require('fs');
 
 
-functions.http('googleapis', (req, res) => {
-  console.log({google})
-  res.send(`Hello ${req.query.name || req.body.name || 'World TEST CHANGE 3uelkfjsldkfjsdkjf'}!`);
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get('/gmail', async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: ['https://www.googleapis.com/auth/gmail.readonly'],
+  });
+  const client = await auth.getClient();
+  const gmail = google.gmail({ version: 'v1', auth: client });
+  const messages = await gmail.users.messages.list({
+    userId: 'me',
+  });
+  res.send(messages.data);
+})
+
+// Define an endpoint that must be included in the body of the request
+app.get('/', (req, res) => {
+  fs.readFile('welcome.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading file');
+      return;
+    }
+    res.send(data);
+  });
 });
+
+functions.http('googleapis', app);
+
+
