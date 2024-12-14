09532d5a0cfe1755c79a5b74e369d13b9e7c6918
William Owen <william.owen.bsc@gmail.com>
2024-12-14 11:09:57 -0800

    Add documentation comments to logIf module for clarity on usage in full-stack application

--- dist/components.js
+++ dist/components.js
@@ -905,7 +905,21 @@ function evaluateTemplate(template, context) {
   \************************/
 /***/ ((module, exports) => {
 
-var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var logIf = {
+var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/**
+ * This module is used to control the logging of different parts of the application.
+ * 
+ * Because the application is a full-stack application, it can be difficult to debug the application
+ * because the logs from the server and the client are mixed together.
+ * 
+ * Backend JS and Client js use different import styles, so we need to use a special export style to make this module work in both environments.
+ * 
+ * in files you'll see code like this:
+ * const logIf = require('../../../utils/logIf.js'); <--- this is a node.js import
+ * import '../../../utils/logIf.js'; <--- this is a client import and binds logIf to the window object
+ * 
+ */
+
+var logIf = {
   server: true,
   client: false,
   archive: false,
--- utils/logIf.js
+++ utils/logIf.js
@@ -1,3 +1,18 @@
+/**
+ * This module is used to control the logging of different parts of the application.
+ * 
+ * Because the application is a full-stack application, it can be difficult to debug the application
+ * because the logs from the server and the client are mixed together.
+ * 
+ * Backend JS and Client js use different import styles, so we need to use a special export style to make this module work in both environments.
+ * 
+ * in files you'll see code like this:
+ * const logIf = require('../../../utils/logIf.js'); <--- this is a node.js import
+ * import '../../../utils/logIf.js'; <--- this is a client import and binds logIf to the window object
+ * 
+ */
+
+
 const logIf = {
     server: true,
     client: false,
