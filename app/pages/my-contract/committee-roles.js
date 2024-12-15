34234c50c2b2781f7efdf8421b86ee1890d89be8
William Owen <william.owen.bsc@gmail.com>
2024-12-15 14:01:03 -0800

    Add new committee roles for Event Coordination and Volunteer Management; update responsibilities

--- app/pages/my-contract/committee-roles.js
+++ app/pages/my-contract/committee-roles.js
@@ -6,6 +6,7 @@ const roles = {
         responsibility: 'Oversee all aspects of the organization',
         prerequisite: 'Previous experience in a leadership role',
         tasks: [],
+        sideBarButtons: []
     },
     2: {
         title: 'Vice President',
@@ -13,6 +14,7 @@ const roles = {
         responsibility: 'Assist the President and oversee the committees',
         prerequisite: 'Experience in organizational management',
         tasks: [],
+        sideBarButtons: []
     },
     3: {
         title: 'Treasurer',
@@ -20,6 +22,7 @@ const roles = {
         responsibility: "Manage the organization's finances",
         prerequisite: 'Experience in financial management',
         tasks: [],
+        sideBarButtons: []
     },
     4: {
         title: 'Secretary',
@@ -27,6 +30,7 @@ const roles = {
         responsibility: 'Record and distribute meeting minutes',
         prerequisite: 'Strong organizational and communication skills',
         tasks: [],
+        sideBarButtons: []
     },
     5: {
         title: 'Member-at-large',
@@ -34,6 +38,7 @@ const roles = {
         responsibility: 'Assist with various tasks as needed',
         prerequisite: 'Willingness to assist with various tasks',
         tasks: [],
+        sideBarButtons: []
     },
     6: {
         title: 'Web Design/Maintenance Chair',
@@ -41,6 +46,7 @@ const roles = {
         responsibility: "Maintain the organization's website",
         prerequisite: 'Experience in web design and maintenance',
         tasks: [],
+        sideBarButtons: []
     },
     7: {
         title: 'Social Media Chair',
@@ -48,6 +54,7 @@ const roles = {
         responsibility: "Manage the organization's social media accounts",
         prerequisite: 'Experience in social media management',
         tasks: [],
+        sideBarButtons: []
     },
     8: {
         title: 'Design general GHOST posts (using Canva)',
@@ -55,6 +62,7 @@ const roles = {
         responsibility: "Create posts for the organization's social media accounts",
         prerequisite: 'Experience with Canva and social media design',
         tasks: [],
+        sideBarButtons: []
     },
     9: {
         title: 'Design mosaics of individual artists (using Canva)',
@@ -62,6 +70,7 @@ const roles = {
         responsibility: "Create posts for the organization's social media accounts",
         prerequisite: 'Experience with Canva and social media design',
         tasks: [],
+        sideBarButtons: []
     },
     10: {
         title: 'Design Sponsor and "Make It a Weekend" posts (using Canva)',
@@ -69,6 +78,7 @@ const roles = {
         responsibility: "Create posts for the organization's social media accounts",
         prerequisite: 'Experience with Canva and social media design',
         tasks: [],
+        sideBarButtons: []
     },
     11: {
         title: 'Prepare short videos',
@@ -76,6 +86,7 @@ const roles = {
         responsibility: 'Create posts for the organization',
         prerequisite: 'Experience in video creation and editing',
         tasks: [],
+        sideBarButtons: []
     },
     12: {
         title: 'Artist Images Chair',
@@ -83,6 +94,7 @@ const roles = {
         responsibility: 'Take photos of artists',
         prerequisite: 'Experience in photography',
         tasks: [],
+        sideBarButtons: []
     },
     13: {
         title: 'Brochure and Poster Chair',
@@ -90,6 +102,7 @@ const roles = {
         responsibility: 'Design brochures and posters',
         prerequisite: 'Experience in graphic design',
         tasks: [],
+        sideBarButtons: []
     },
     14: {
         title: 'Brochure and Poster Designer',
@@ -97,6 +110,7 @@ const roles = {
         responsibility: 'Design brochures and posters',
         prerequisite: 'Experience in graphic design',
         tasks: [],
+        sideBarButtons: []
     },
     15: {
         title: 'New Artist Recruitment Chair',
@@ -104,6 +118,7 @@ const roles = {
         responsibility: 'Recruit new artists',
         prerequisite: 'Experience in recruitment',
         tasks: [],
+        sideBarButtons: []
     },
     16: {
         title: 'New Artist Recruitment Assistant',
@@ -111,6 +126,7 @@ const roles = {
         responsibility: 'Recruit new artists',
         prerequisite: 'Experience in recruitment',
         tasks: [],
+        sideBarButtons: []
     },
     17: {
         title: 'New Artist Applications Chair',
@@ -118,6 +134,7 @@ const roles = {
         responsibility: 'Review new artist applications',
         prerequisite: 'Experience in reviewing applications',
         tasks: [],
+        sideBarButtons: []
     },
     18: {
         title: 'Distribution Chair',
@@ -125,6 +142,7 @@ const roles = {
         responsibility: 'Distribute brochures and posters',
         prerequisite: 'Experience in distribution',
         tasks: [],
+        sideBarButtons: []
     },
     19: {
         title: 'Distribution Assistant',
@@ -132,6 +150,7 @@ const roles = {
         responsibility: 'Distribute brochures and posters to Gig Harbor (Downtown, Pioneer Ave and Kimball Ave)',
         prerequisite: 'Experience in distribution',
         tasks: [],
+        sideBarButtons: []
     },
     20: {
         title: 'Distribution Assistant',
@@ -139,6 +158,7 @@ const roles = {
         responsibility: 'Distribute brochures and posters to Gig Harbor (Costco corridor and Point Fosdick business corridor)',
         prerequisite: 'Experience in distribution',
         tasks: [],
+        sideBarButtons: []
     },
     21: {
         title: 'Distribution Assistant',
@@ -146,6 +166,7 @@ const roles = {
         responsibility: 'Distribute brochures and posters to Port Orchard/Olalla (including on the Southworth ferry)',
         prerequisite: 'Experience in distribution',
         tasks: [],
+        sideBarButtons: []
     },
     22: {
         title: 'Distribution Assistant',
@@ -153,6 +174,7 @@ const roles = {
         responsibility: 'Distribute brochures and posters to selected areas in Seattle, Bellevue and Mercer Island',
         prerequisite: 'Experience in distribution',
         tasks: [],
+        sideBarButtons: []
     },
     23: {
         title: 'Distribution Assistant',
@@ -160,6 +182,7 @@ const roles = {
         responsibility: 'Distribute brochures and posters to selected areas in Bremerton (including on the ferry), Bainbridge Island, Poulsbo and Port Townsend',
         prerequisite: 'Experience in distribution',
         tasks: [],
+        sideBarButtons: []
     },
     24: {
         title: 'Sponsorship Chair',
@@ -167,6 +190,7 @@ const roles = {
         responsibility: 'Oversee sponsorship activities and relationships with sponsors (including paid ads)',
         prerequisite: 'Experience in sponsorship management',
         tasks: [],
+        sideBarButtons: []
     },
     25: {
         title: 'Sponsorship Assistant',
@@ -174,6 +198,7 @@ const roles = {
         responsibility: 'Assist with sponsorship activities and maintain relationships with sponsors (including paid ads)',
         prerequisite: 'Experience in sponsorship management',
         tasks: [],
+        sideBarButtons: []
     },
     26: {
         title: 'Signs for Artist Studios Chair',
@@ -181,6 +206,7 @@ const roles = {
         responsibility: 'Oversee the creation and distribution of signs for artist studios',
         prerequisite: 'Experience in sign creation and distribution',
         tasks: [],
+        sideBarButtons: []
     },
     27: {
         title: 'Signs for Artist Studios Assistant',
@@ -188,6 +214,7 @@ const roles = {
         responsibility: 'Sign repairs plus bundling signs for distribution and help collecting signs at end of year',
         prerequisite: 'Experience in sign creation and distribution',
         tasks: [],
+        sideBarButtons: []
     },
     28: {
         title: 'Signs for Artist Studios Assistant',
@@ -195,6 +222,7 @@ const roles = {
         responsibility: 'Sign repairs plus bundling signs for distribution and help collecting signs at end of year',
         prerequisite: 'Experience in sign creation and distribution',
         tasks: [],
+        sideBarButtons: []
     },
     29: {
         title: 'Banners Chair',
@@ -202,6 +230,7 @@ const roles = {
         responsibility: 'Oversee the creation and distribution of banners',
         prerequisite: 'Experience in banner creation and distribution',
         tasks: [],
+        sideBarButtons: []
     },
     30: {
         title: 'Banners Assistant',
@@ -209,6 +238,7 @@ const roles = {
         responsibility: 'Assist with the creation and distribution of banners',
         prerequisite: 'Experience in banner creation and distribution',
         tasks: [],
+        sideBarButtons: []
     },
     31: {
         title: 'General Advertising Chair',
@@ -216,6 +246,7 @@ const roles = {
         responsibility: 'Oversee all general advertising activities',
         prerequisite: 'Experience in advertising',
         tasks: [],
+        sideBarButtons: []
     },
     32: {
         title: 'General Advertising Distributor',
@@ -223,6 +254,7 @@ const roles = {
         responsibility: 'Install 4-5 signs in Gig Harbor (Downtown, Pioneer Ave and Kimball Ave) then collect them after the tour – early August',
         prerequisite: 'Experience in advertising',
         tasks: [],
+        sideBarButtons: []
     },
     33: {
         title: 'General Advertising Distributor',
@@ -230,6 +262,7 @@ const roles = {
         responsibility: 'Install 4-5 signs in Gig Harbor (Costco and Point Fosdick business corridors) then collect them after the tour – early August',
         prerequisite: 'Experience in advertising',
         tasks: [],
+        sideBarButtons: []
     },
     34: {
         title: 'General Advertising Distributor',
@@ -237,6 +270,7 @@ const roles = {
         responsibility: 'Install 4-5 signs in Port Orchard/Olalla (including near the Southworth ferry) then collect them after the tour – early August',
         prerequisite: 'Experience in advertising',
         tasks: [],
+        sideBarButtons: []
     },
     35: {
         title: 'General Advertising Distributor',
@@ -244,6 +278,7 @@ const roles = {
         responsibility: 'Install 4-5 signs in selected areas in Tacoma and Olympia then collect them after the tour – early August',
         prerequisite: 'Experience in advertising',
         tasks: [],
+        sideBarButtons: []
     },
     36: {
         title: 'General Advertising Distributor',
@@ -251,6 +286,7 @@ const roles = {
         responsibility: 'Install 4-5 signs in selected areas in Seattle, Bellevue and Mercer Island then collect them after the tour – early August',
         prerequisite: 'Experience in advertising',
         tasks: [],
+        sideBarButtons: []
     },
     37: {
         title: 'General Advertising Distributor',
@@ -258,6 +294,7 @@ const roles = {
         responsibility: 'Install 4-5 signs in selected areas in Bremerton (including on the ferry), Bainbridge Island, Poulsbo and Port Townsend then collect them after the tour – early August',
         prerequisite: 'Experience in advertising',
         tasks: [],
+        sideBarButtons: []
     },
     38: {
         title: 'Mailing List Coordinator',
@@ -265,6 +302,7 @@ const roles = {
         responsibility: 'Manage the mailing list and send out newsletters using Mail Chimp',
         prerequisite: 'Experience in mailing list management',
         tasks: [],
+        sideBarButtons: []
     },
     39: {
         title: 'Advertising/Publicity Chair',
@@ -272,6 +310,7 @@ const roles = {
         responsibility: 'Oversee all advertising and publicity activities',
         prerequisite: 'Experience in advertising and publicity',
         tasks: [],
+        sideBarButtons: []
     },
     40: {
         title: 'Information Booth Chair',
@@ -279,6 +318,7 @@ const roles = {
         responsibility: 'Oversee the setup and operation of the information booth',
         prerequisite: 'Experience in event management',
         tasks: [],
+        sideBarButtons: []
     },
     41: {
         title: 'Information Booth Assistant',
@@ -286,6 +326,7 @@ const roles = {
         responsibility: 'Assist chair with booth set up and tear down',
         prerequisite: 'Experience in event management',
         tasks: [],
+        sideBarButtons: []
     },
     42: {
         title: 'Information Booth Volunteer',
@@ -293,6 +334,7 @@ const roles = {
         responsibility: 'Man the booth Friday, July 18th',
         prerequisite: 'Willingness to volunteer',
         tasks: [],
+        sideBarButtons: []
     },
     43: {
         title: 'Information Booth Volunteer',
@@ -300,6 +342,7 @@ const roles = {
         responsibility: 'Man the booth Friday, July 19th',
         prerequisite: 'Willingness to volunteer',
         tasks: [],
+        sideBarButtons: []
     },
     44: {
         title: 'Information Booth Volunteer',
@@ -307,8 +350,11 @@ const roles = {
         responsibility: 'Man the booth Friday, July 20th',
         prerequisite: 'Willingness to volunteer',
         tasks: [],
+        sideBarButtons: []
     },
 }
+
+export default roles
 
 
 export default roles
