aa53fdbf34c14e7033e786442c7bcd8e8bbe393b
William Owen <william.owen.bsc@gmail.com>
2024-12-21 11:29:15 -0800

    Refactor invoice email layout for improved readability and presentation

--- app/pages/members/index.js
+++ app/pages/members/index.js
@@ -149,20 +149,15 @@ function testEmailButton(role){
         <div style="text-align:center">
         <h1>Congratulations on joining the <br>Gig Harbor Open Studio Tour</h1>
             <p>Here is your invoice for the membership fee</p>
-          
             <fieldset style="width:fit-content; margin: auto;">
-            
             <legend>Invoice</legend>
-            
             <p style="margin:0;">Transaction ID: 123</p>
             <p style="margin:0;">Amount: 123}</p>
             <p style="margin:0;">Currency:USD</p>
             <p style="margin:0;">Status: complete </p>
             <p style="margin:0;">Created At: ${new Date().toLocaleString()}</p>
             </fieldset>
-
             <p>Thank you for your membership payment.</p>
-            
             <p>Best Regards, <br>Gig Harbor Open Studio Tour</p>
         <div>
             `
