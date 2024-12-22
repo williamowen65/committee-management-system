2df276c5426c65cb98d54b7357701d56e0fbaf4a
William Owen <william.owen.bsc@gmail.com>
2024-12-21 18:31:49 -0800

    Log response from sheetsController in testSheetsButton function

--- app/pages/members/index.js
+++ app/pages/members/index.js
@@ -199,6 +199,7 @@ function testSheetsButton(role) {
           alert('Error generating sheets')
           return
         }
+        console.log("sheetsController-response", event)
         // You can add additional logic here to handle the message
         // show success message
         alert('Sheets generated successfully: ' + event.data.sheetUrl)
