98c13e00c6539e5a90718ca9a1ac3ebba3b07d39
William Owen <william.owen.bsc@gmail.com>
2024-12-25 11:34:19 -0800

    Refactor new application form by removing commented-out code and unnecessary debugger statements

--- app/pages/new-application/index.js
+++ app/pages/new-application/index.js
@@ -39,62 +39,53 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
         Promise.all(promises).then(() => {  
-            debugger
             CRUD.create('new-applications', values).then(() => {
 
-                // get all board members
-                // plus new artist applications chair
-                // new artist recruitment chair
+                // const newArtist = values
+                // // get all board members
+                // // plus new artist applications chair
+                // // new artist recruitment chair
 
-                window.sendMessageToParent({
-                    controller: 'gmailController',
-                    // every one on board, new artist applications chair, new artist recruitment chair and the person who applied
-                //   to: ""
-                    // to: 'bluekayak123@yahoo.com, william.owen.dev@gmail.com', // <---TESTING
-                    subject: `GHOST Contract Application sent from ${newArtist.name}`,
-                    body: `
-                     <div style="text-align:center">
-                      <h1>The Gig Harbor Open Studio Tour is Growing</h1>
-                        <h2>New Artist Application Received</h2>
-                        <p>Artist Name: ${newArtist.name}</p>
-                        <p>Artist Email: ${newArtist.email}</p>
-                        <p>Artist Phone: ${newArtist.phone}</p>
-                        <p>Artist Address: ${newArtist.address}</p>
-                        <p>Artist Website: ${newArtist.website}</p>
-                        <p>Artist Media: ${newArtist.media}</p>
-                        <p>Artist Description: ${newArtist.description}</p>
-                        <p>Artist Image: <img src="${newArtist.image}" alt="Artist Image" style="max-width: 100%;"></p>
-                        <p>Artist Image 2: <img src="${newArtist.image2}" alt="Artist Image 2" style="max-width: 100%;"></p>
-                        <p>Artist Image 3: <img src="${newArtist.image3}" alt="Artist Image 3" style="max-width: 100%;"></p>
-                        <p>Artist Image 4: <img src="${newArtist.image4}" alt="Artist Image 4" style="max-width: 100%;"></p>
-                        <p>Artist Image 5: <img src="${newArtist.image5}" alt="Artist Image 5" style="max-width: 100%;"></p>
+                // window.sendMessageToParent({
+                //     controller: 'gmailController',
+                //     // every one on board, new artist applications chair, new artist recruitment chair and the person who applied
+                // //   to: ""
+                //     // to: 'bluekayak123@yahoo.com, william.owen.dev@gmail.com', // <---TESTING
+                //     subject: `GHOST New Artist Application sent from ${newArtist.firstName} ${newArtist.lastName}`,
+                //     body: `
+                //      <div style="text-align:center">
+                //       <h1>A GHOST New Artist Application has been submitted!</h1>
+                //         <p>The committee will be reviewing the application and new artist application should receive a notification 
+                        
+                //         </p>
 
 
 
-                    </div>
-                      `
-                  })
+
+                //     </div>
+                //       `
+                //   })
     
     
     
-                  window.addEventListener("message", (event) => {
-                    // if (event.data.dispatch !== 'gmailController-response') return
+                //   window.addEventListener("message", (event) => {
+                //     // if (event.data.dispatch !== 'gmailController-response') return
     
-                    // if (event.data.error) {
-                    //   document.querySelector('.processPayment').innerText = "<div style='color:red; font-weight: bold'>Payment successful but error sending email</div>"
-                    //   return
-                    // }
+                //     // if (event.data.error) {
+                //     //   document.querySelector('.processPayment').innerText = "<div style='color:red; font-weight: bold'>Payment successful but error sending email</div>"
+                //     //   return
+                //     // }
     
-                    // // You can add additional logic here to handle the message
-                    // // show success message
+                //     // // You can add additional logic here to handle the message
+                //     // // show success message
     
-                    // document.querySelector('.processPayment').innerText = "Payment Successful - Email Sent"
+                //     // document.querySelector('.processPayment').innerText = "Payment Successful - Email Sent"
     
-                    // setTimeout(() => {
-                    //   // redirect to the dashboard
-                    //   window.location.href = '/members'
-                    // }, 3000)
-                  });
+                //     // setTimeout(() => {
+                //     //   // redirect to the dashboard
+                //     //   window.location.href = '/members'
+                //     // }, 3000)
+                //   });
 
             }).then(() => {
                 const button = document.querySelector('button[type="submit"]')
