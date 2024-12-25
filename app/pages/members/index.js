b1ad3fd57b8a0ab2dfdcc1c34a0bad6eaee5df26
William Owen <william.owen.bsc@gmail.com>
2024-12-25 11:02:26 -0800

    Update sheet name format in textProcessToSheetsButton to use 'Current Year'

--- app/pages/members/index.js
+++ app/pages/members/index.js
@@ -419,7 +419,7 @@ function textProcessToSheetsButton(role) {
   setTimeout(() => {
     document.getElementById('processToSheets').addEventListener('click', async () => {
 
-      const sheetName = `GHOST Contracts ${new Date().getFullYear()}`
+      const sheetName = `GHOST Contracts Current Year`
       if (!sheetName) return
 
       // get the text of the button
