196906119d9dd1f93aa8d10c5af92136c286aa4f
William Owen <william.owen.bsc@gmail.com>
2024-12-26 13:50:06 -0800

    Update sendNewApplicationEmail to improve date formatting and enhance notification message clarity

--- app/outbound-emails/newApplicationReceived.js
+++ app/outbound-emails/newApplicationReceived.js
@@ -8,20 +8,28 @@ export async function sendNewApplicationEmail(values){
     const newApplicationEventId = '1Iig2hZFN7MgyFUpjG0q' // A timeline event id for some date in the future
     const specialTimelineEvent  = await CRUD.read('ghost-timeline', newApplicationEventId)
 
+    let date;
+    if(typeof specialTimelineEvent.date === 'string'){
+
+        date = new Date(specialTimelineEvent.date).toLocaleDateString('en-us', {month: 'long', day: 'numeric'})
+    } else {
+        date = specialTimelineEvent.date.toDate().toLocaleDateString('en-us', {month: 'long', day: 'numeric'})
+    }
+
     window.sendMessageToParent({
         controller: 'gmailController',
         // every one on board, new artist applications chair, new artist recruitment chair and the person who applied
     //   to: ""
-        to: 'bluekayak123@yahoo.com, william.owen.dev@gmail.com', // <---TESTING
+        to: 'william.owen.dev@gmail.com', // <---TESTING
         subject: `GHOST New Artist Application sent from ${newArtist.firstName} ${newArtist.lastName}`,
         body: `
          <div style="text-align:center">
           <h1>A GHOST New Artist Application has been submitted!</h1>
             <p>
-            The committee will be reviewing the application.<br>
-            The new artist applicant, ${newArtist.firstName} ${newArtist.lastName}, will receive a notification after the application has been reviewed. 
-            <br>
-            Application must be reviewed no later than ${specialTimelineEvent.date.toDate().toLocaleDateString('en-us', {month: 'long', day: 'numeric'})}.
+            The committee will be reviewing the application from <b>${newArtist.firstName} ${newArtist.lastName}</b>.<br>
+            The new artist applicant will receive a notification once the application has been reviewed. 
+            </p>
+            <p>Application must be reviewed no later than ${date}.
             </p>
 
 
