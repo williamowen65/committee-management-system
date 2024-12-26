8f87486e15c239371872136df7f548a797a61386
William Owen <william.owen.bsc@gmail.com>
2024-12-26 13:27:44 -0800

    Add email functions for new contract submission notifications

--- app/components/3rdParty/paypal/index.js
+++ app/components/3rdParty/paypal/index.js
@@ -1,6 +1,7 @@
 import paypalTemplate from './index.html.txt';
 import { createCustomElement, evaluateTemplate } from '../../../../utils/custom-element';
 import roles from '../../../pages/my-contract/committee-roles.js';
+import { sendNewContractSubmissionBoardEmail, sendNewContractSubmissionEmail } from '../../../outbound-emails/newContractSubmission.js';
 // import styles from './style.scss.txt';
 const logIf = require("../../../../utils/logIf.js");
 
@@ -191,92 +192,12 @@ window.initializePaypalButtons = function (cost = 250.00) {
 
               // send email to the user
 
+              sendNewContractSubmissionEmail(user, transaction)
 
-              const personalEmail = user.artistDetails.personalEmail || ""
-              const businessEmail = user.artistDetails.businessEmail || ""
+              // send email to the board members
 
-              window.sendMessageToParent({
-                controller: 'gmailController',
-                to: `${email}, ${personalEmail}, ${businessEmail}`,
-                subject: 'GHOST Contract Invoice',
-                body: `
-                 <div style="text-align:center">
-                  <h1>Congratulations on joining the Gig Harbor Open Studio Tour</h1>
-                  <p>Here is your invoice for the membership fee. 
-                  <br> Your contract has been submitted. 
-                  <br> You will be able to make changes to the contract up until March 2nd, 2025
-                   </p>
-                
-                  <fieldset style="width:fit-content; margin:auto;">
-                  
-                  <legend>Invoice</legend>
-                  
-                  <p style="margin:0; text-align:start;">Transaction ID: ${transaction.id}</p>
-                  <p style="margin:0; text-align:start;">Amount: ${transaction.amount.value}</p>
-                  <p style="margin:0; text-align:start;">Currency: ${transaction.amount.currency_code}</p>
-                  <p style="margin:0; text-align:start;">Status: ${transaction.status}</p>
-                  <p style="margin:0; text-align:start;">Created At: ${new Date().toLocaleString()}</p>
-                  </fieldset>
+              sendNewContractSubmissionBoardEmail(user, transaction, contracts)
 
-                  <p>Thank you for your membership payment.</p>
-                   <p>Best Regards, <br>Gig Harbor Open Studio Tour</p>
-                </div>
-                  `
-              })
-
-              const newArtist = {
-                name: `${user.artistDetails.firstName} ${user.artistDetails.lastName}`,
-                email: email
-              }
-              const ghostBoardMemberRoleKeys = Object.entries(roles).filter(([key,role]) => {
-                if(role.committee == 'Board') return key
-              }).filter(Boolean)
-
-              const ghostArtistImagesChairRoleKeys = Object.entries(roles).filter(([key,role]) => {
-                if(role.title == 'Artist Images Chair') return key
-              }).filter(Boolean)
-
-              const ghostBoardMemberEmails = contracts.filter(contract => {
-                if(ghostBoardMemberRoleKeys.includes(contract.role)) return contract.artistDetails.personalEmail
-              }).filter(Boolean)
-
-              const ghostArtistImagesChairEmails = contracts.filter(contract => {
-                if(ghostArtistImagesChairRoleKeys.includes(contract.role)) return contract.artistDetails.personalEmail
-              }).filter(Boolean)
-
-              // To all the board member and artist images chair emails
-              const welcomeEmailAddress = ghostBoardMemberEmails.concat(ghostArtistImagesChairEmails).join(',')
-
-              window.sendMessageToParent({
-                controller: 'gmailController',
-                // To all the board member and artist images chair emails
-                to: welcomeEmailAddress,
-                // to: 'bluekayak123@yahoo.com, william.owen.dev@gmail.com', // <---TESTING
-                subject: `GHOST Contract Payment Submitted by ${newArtist.name}`,
-                body: `
-                 <div style="text-align:center">
-                  <h1>The Gig Harbor Open Studio Tour is Growing</h1>
-                  <p>${newArtist.name} has submitted their membership payment.
-                  <br> Reach out to them to welcome them to the tour.
-                  <br> You can reach them at ${newArtist.email}
-                   </p>
-                
-                  <fieldset style="width:fit-content; margin:auto;">
-                  
-                  <legend>Invoice</legend>
-                  
-                  <p style="margin:0; text-align:start;">Transaction ID: ${transaction.id}</p>
-                  <p style="margin:0; text-align:start;">Amount: ${transaction.amount.value}</p>
-                  <p style="margin:0; text-align:start;">Currency: ${transaction.amount.currency_code}</p>
-                  <p style="margin:0; text-align:start;">Status: ${transaction.status}</p>
-                  <p style="margin:0; text-align:start;">Created At: ${new Date().toLocaleString()}</p>
-                  </fieldset>
-
-                  <p>Thank you for your membership.</p>
-                   <p>Best Regards, <br>Gig Harbor Open Studio Tour</p>
-                </div>
-                  `
-              })
 
 
 
---
+++ app/outbound-emails/newContractSubmission.js
@@ -0,0 +1,91 @@
+export function sendNewContractSubmissionEmail(user, transaction){
+    
+    const personalEmail = user.artistDetails.personalEmail || ""
+    const businessEmail = user.artistDetails.businessEmail || ""
+
+    window.sendMessageToParent({
+      controller: 'gmailController',
+      to: `${email}, ${personalEmail}, ${businessEmail}`,
+      subject: 'GHOST Contract Invoice',
+      body: `
+       <div style="text-align:center">
+        <h1>Congratulations on joining the Gig Harbor Open Studio Tour</h1>
+        <p>Here is your invoice for the membership fee. 
+        <br> Your contract has been submitted. 
+        <br> You will be able to make changes to the contract up until March 2nd, 2025
+         </p>
+      
+        <fieldset style="width:fit-content; margin:auto;">
+        
+        <legend>Invoice</legend>
+        
+        <p style="margin:0; text-align:start;">Transaction ID: ${transaction.id}</p>
+        <p style="margin:0; text-align:start;">Amount: ${transaction.amount.value}</p>
+        <p style="margin:0; text-align:start;">Currency: ${transaction.amount.currency_code}</p>
+        <p style="margin:0; text-align:start;">Status: ${transaction.status}</p>
+        <p style="margin:0; text-align:start;">Created At: ${new Date().toLocaleString()}</p>
+        </fieldset>
+
+        <p>Thank you for your membership payment.</p>
+         <p>Best Regards, <br>Gig Harbor Open Studio Tour</p>
+      </div>
+        `
+    })
+}
+
+export function sendNewContractSubmissionBoardEmail(user, transaction, contracts){
+    
+    const newArtist = {
+        name: `${user.artistDetails.firstName} ${user.artistDetails.lastName}`,
+        email: email
+      }
+      const ghostBoardMemberRoleKeys = Object.entries(roles).filter(([key,role]) => {
+        if(role.committee == 'Board') return key
+      }).filter(Boolean)
+
+      const ghostArtistImagesChairRoleKeys = Object.entries(roles).filter(([key,role]) => {
+        if(role.title == 'Artist Images Chair') return key
+      }).filter(Boolean)
+
+      const ghostBoardMemberEmails = contracts.filter(contract => {
+        if(ghostBoardMemberRoleKeys.includes(contract.role)) return contract.artistDetails.personalEmail
+      }).filter(Boolean)
+
+      const ghostArtistImagesChairEmails = contracts.filter(contract => {
+        if(ghostArtistImagesChairRoleKeys.includes(contract.role)) return contract.artistDetails.personalEmail
+      }).filter(Boolean)
+
+      // To all the board member and artist images chair emails
+      const welcomeEmailAddress = ghostBoardMemberEmails.concat(ghostArtistImagesChairEmails).join(',')
+
+      window.sendMessageToParent({
+        controller: 'gmailController',
+        // To all the board member and artist images chair emails
+        to: welcomeEmailAddress,
+        // to: 'bluekayak123@yahoo.com, william.owen.dev@gmail.com', // <---TESTING
+        subject: `GHOST Contract Payment Submitted by ${newArtist.name}`,
+        body: `
+         <div style="text-align:center">
+          <h1>The Gig Harbor Open Studio Tour is Growing</h1>
+          <p>${newArtist.name} has submitted their membership payment.
+          <br> Reach out to them to welcome them to the tour.
+          <br> You can reach them at ${newArtist.email}
+           </p>
+        
+          <fieldset style="width:fit-content; margin:auto;">
+          
+          <legend>Invoice</legend>
+          
+          <p style="margin:0; text-align:start;">Transaction ID: ${transaction.id}</p>
+          <p style="margin:0; text-align:start;">Amount: ${transaction.amount.value}</p>
+          <p style="margin:0; text-align:start;">Currency: ${transaction.amount.currency_code}</p>
+          <p style="margin:0; text-align:start;">Status: ${transaction.status}</p>
+          <p style="margin:0; text-align:start;">Created At: ${new Date().toLocaleString()}</p>
+          </fieldset>
+
+          <p>Thank you for your membership.</p>
+           <p>Best Regards, <br>Gig Harbor Open Studio Tour</p>
+        </div>
+          `
+      })
+}
