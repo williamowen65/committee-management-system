2a07ae5bbffc7c1a41f782fb4e002b70dea63053
William Owen <william.owen.bsc@gmail.com>
2024-12-26 09:58:18 -0800

    Re-add event listener for Cancel button to exit edit mode in members form

--- app/pages/members/index.js
+++ app/pages/members/index.js
@@ -186,13 +186,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
               `
 
-          const cancelEditButton = editForm.querySelector('.cancelTimelineEdit');
-          if (cancelEditButton) {
-            cancelEditButton.addEventListener('click', (e) => {
-              // change out of edit mode
-              e.target.closest('li').removeAttribute('is-editing');
-            });
-          }
+        
 
 
           // make a clone of the form to add to the timeline
@@ -262,6 +256,16 @@ document.addEventListener('DOMContentLoaded', function () {
             event.appendChild(editFormClone)
 
           })
+
+
+
+          const cancelEditButton = editForm.querySelector('.cancelTimelineEdit');
+          if (cancelEditButton) {
+            cancelEditButton.addEventListener('click', (e) => {
+              // change out of edit mode
+              e.target.closest('li').removeAttribute('is-editing');
+            });
+          }
 
         }
 
